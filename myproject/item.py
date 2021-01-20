374668794aa2801c665e3a15ec6f48c000bd1aad
A K SINGH <adityendra97@gmail.com>
2021-01-20 13:04:29 +0530

    item file is modified

--- myproject/item.py
+++ myproject/item.py
@@ -40,6 +40,15 @@ class Item(Resource):
 
         item = {'name': name, 'price': data['price']}
 
+        try:
+            self.insert(item)
+        except:
+            return {"message": "An error occurred inserting the item."}, 500
+
+        return item, 201
+
+    @classmethod
+    def insert(cls, item):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
@@ -48,7 +57,6 @@ class Item(Resource):
 
         connection.commit()
         connection.close()
-        return item, 201
 
     def delete(self, name):
         connection = sqlite3.connect('data.db')
@@ -64,15 +72,44 @@ class Item(Resource):
     def put(self, name):
         data = Item.parser.parse_args()
 
-        item = next(filter(lambda x: x['name'] == name, items), None)
+        item = self.find_by_name(name)
+        update_item = {'name': name, 'price': data['price']}
         if item is None:
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
+            try:
+                self.insert(update_item)
+            except:
+                return {'message': "An error occurred inserting the item"}, 500
         else:
-            item.update(data)
-        return item
+            try:
+                self.insert(update_item)
+            except:
+                return {'message': "An error occurred updating the item"}, 500
+        return update_item
+
+    @classmethod
+    def update(cls, item):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "UPDATE items SET price=? WHERE name=?"
+        cursor.execute(query, (item['price'], item['name']))
+
+        connection.commit()
+        connection.close()
 
 
 class ItemList(Resource):
     def get(self):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM items"
+        result = cursor.execute(query)
+        items = []
+        for row in result:
+            items.append({'name': row[0], 'price': row[1]})
+
+        connection.close()
+        connection.close()
+
         return {'items': items}
